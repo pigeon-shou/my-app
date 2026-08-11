be6f3e6b5eecab6a3ec61561a9269181d732a80c
pigeon-shou <pigeonshow2024@gmail.com>
2026-08-11 23:36:43 +0900

    削除　更新機能の追加

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -58,4 +58,38 @@ app.get('/api/categories', async (c) => {
   return c.json(categories)
 })
 
+app.get('/api/subscriptions/:id', async (c) => {
+  const id = Number(c.req.param('id'))
+  const subscription = await prisma.subscription.findUnique({
+    where: { id },
+  })
+  return c.json(subscription)
+})
+
+app.put('/api/subscriptions/:id', async (c) => {
+  const id = Number(c.req.param('id'))
+  const body = await c.req.json()
+
+  const subscription = await prisma.subscription.update({
+    where: { id },
+    data: {
+      name: body.name,
+      price: body.price,
+      cancelDeadline: new Date(body.cancelDeadline),
+      categoryId: body.categoryId,
+    },
+  })
+
+  return c.json(subscription)
+})
+
+app.delete('/api/subscriptions/:id', async (c) => {
+  const id = Number(c.req.param('id'))
+
+  await prisma.subscription.delete({
+    where: { id },
+  })
+
+  return c.body(null, 204)
+})
 export default app
